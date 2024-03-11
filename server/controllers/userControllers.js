465766805b0d486db3bac8eb4582f2f5fa0afb91
Sumit Tiwary <sumitkr9199@gmail.com>
2024-03-11 17:30:14 +0530

    M

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,7 +1,7 @@
 const User = require('../models/userModel')
 const HttpError = require("../models/errorModel")
 const bcrypt=require('bcryptjs')
-
+const jwt =require('jsonwebtoken')
 
 //===========REGISTER NEW USER========//
 // POST : api/users/register
@@ -39,8 +39,30 @@ const registerUser=async (req,res,next)=>{
 //UNPROTECTED
 
 const loginUser=async (req,res,next)=>{
-    res.json("login user")
-}
+    
+try {
+    const {email,password}=req.body;
+    if(!email || !password){
+        return next(new HttpError("Fill in the all fields", 422))
+    }
+    const newEmail=email.toLowerCase();
+
+    const user=await User.findOne({email:newEmail})
+    if(!user){
+        return next(new HttpError("Invalid credentials",422))
+    }
+
+    const comparePass=await bcrypt.compare(password,user.password)
+    if(!comparePass){
+        return next(new HttpError("Invalid Credentials",422))
+    }
+    const {_id: id,name}=user;
+    const token=jwt.sign({id,name}, process.env.JWT_SECRET,{expiresIn:"1d"})
+
+    res.status(200).json({token,id,name})
+} catch (error) {
+    return next(new HttpError("Login failed. Check your credentials",422))
+}}
 
 //===========USER PROFILE========//
 // POST : api/users/:id
